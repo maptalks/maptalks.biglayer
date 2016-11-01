7d76a4c18f378adb65edd29a9badf6dd48091d47
FuZhenn <fuzhen@maptalks.org>
2016-11-01 22:38:19 +0800

    fix build tasks in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,33 @@
+'use strict';
+
 var gulp   = require('gulp'),
-    browserify = require('gulp-browserify');
+    header = require('gulp-header'),
+    browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var version = require('./package.json').version;
 
-gulp.task('watch', function () {
-   var scriptWatcher = gulp.watch(['src/**/*.js', './gulpfile.js'], ['reload']); // watch the same files in our scripts task
+gulp.task('watch', ['build'], function () {
+   var scriptWatcher = gulp.watch(['src/**/*', './gulpfile.js'], ['build']); // watch the same files in our scripts task
 });
 
-gulp.task('reload', function () {
-    return gulp.src('./src/maptalks.webgl.js')
-        .pipe(browserify({
-          shim : {
-            "maptalks": "global:maptalks"
-          },
-          insertGlobals : true,
-          debug : !gulp.env.production
-        }))
-        .on('prebundle', function(bundle) {
-          bundle.external('maptalks');
-        })
-        .pipe(gulp.dest('./dist/maptalks.webgl.js'))
+// ref:
+// https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-transforms.md
+gulp.task('build', function () {
+    // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: ['./src/maptalks.webgl.js'],
+    debug: false
+  });
+
+  b.external(['maptalks']);
+
+  return b.bundle()
+    .pipe(source('maptalks.webgl.js')) // gives streaming vinyl file object
+    .pipe(buffer()) // <----- convert from streaming to buffered vinyl file object
+    // .pipe(uglify()) // now gulp-uglify works
+    .pipe(header('//version:' + version+'\n'))
+    .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('default', ['watch']);
