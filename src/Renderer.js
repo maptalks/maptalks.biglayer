ce83c4a97b9e7fd4f272a454b3e0b29206bcbd71
axmand <815966508@qq.com>
2017-06-12 14:29:26 +0800

    add 'up' rotateZ illustration

--- src/Renderer.js
+++ src/Renderer.js
@@ -365,7 +365,7 @@ export default class WebglRenderer extends maptalks.renderer.CanvasRenderer {
 
         // when map rotates, camera's up axis is pointing to bearing from south direction of map
         // default [0,1,0] is the Y axis,while the angle of inclination always equal 0
-        // if you want to rotate the map after up an incline,please use rotateZ like this:
+        // if you want to rotate the map after up an incline,please rotateZ like this:
         // let up = new vec3(0,0,0);
         // up.rotateZ(target,radians); 
         const up = [Math.sin(bearing), Math.cos(bearing), 0];
